6d7e88f8bf3bbae8ef176d5f802807349021e5ce
MMirkonsio <mirkovalencia90@gmail.com>
2024-09-05 18:43:31 -0400

    Cambios de precio

--- src/components/juegos.jsx
+++ src/components/juegos.jsx
@@ -113,7 +113,7 @@ function Juegos() {
     },
     {
       title: "Juego Inflable El Tiburón",
-      price: "65.000",
+      price: "60.000",
       images: ["/img/6.webp", "/img/6.1.webp", "/img/6.2.webp"],
     },
     {
@@ -123,7 +123,7 @@ function Juegos() {
     },
     {
       title: "Juego Inflable El Avión",
-      price: "65.000",
+      price: "60.000",
       images: ["/img/8.webp", "/img/8.1.webp", "/img/8.2.webp"],
     },
     {
